bbacd1dcfcfa43c52ba92a5d5e388d0780b8d970
dbasarena <dbasarena@checkpos.com>
2023-03-02 21:06:07 -0300

    First commit

---
+++ Cat.lua
@@ -0,0 +1,30 @@
+Cat = Class{}
+
+local GRAVITY = 20
+
+function Cat:init()
+    self.image = love.graphics.newImage('gigito.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+
+function Cat:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+
+    if love.keyboard.wasPressed('space') then
+      self.dy = -5
+    end
+
+    self.y = self.y + self.dy
+end
+
+
+function Cat:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
